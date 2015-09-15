b6a5cbb6f2eb308cb471038b9bbd75ac56364ede
Adam_D_Scott <ascott@genome.wustl.edu>
2015-09-15 13:13:26 -0500

    water filter when reading ProximityFile.csv

--- lib/TGI/Mutpro/Main/Proximity.pm
+++ lib/TGI/Mutpro/Main/Proximity.pm
@@ -275,6 +275,14 @@ sub getDrugportInfo {
     return \%drugport_hash;
 }
 
+sub filterWater {
+	my ( $residue1 , $residue2 ) = @_;
+	if ( $residue1 =~ /HOH/ || $residue2 =~ /HOH/ ) {
+		return 1;
+	}
+	return 0;
+}
+
 # proximity searching 
 sub proximitySearching {
     my ( $this, $mafHashref, $proximityOutPrefix, $drugportref ) = @_;
@@ -288,6 +296,7 @@ sub proximitySearching {
             my ( $uid1, $chain1, $pdbcor1, $offset1, $residue1, $domain1, $cosmic1, 
                  $uid2, $chain2, $pdbcor2, $offset2, $residue2, $domain2, $cosmic2, 
                  $proximityinfor ) = @ta;
+			if ( &filterWater( $residue1 , $residue2 ) ) { next; }
             my $uniprotcor1 = $pdbcor1 + $offset1; 
             my $uniprotcor2 = $pdbcor2 + $offset2;
             my $lineardis = undef;
@@ -409,19 +418,10 @@ sub drug_proximity_postprocessing {
         chomp; my @t = split /\t/, $_; my ($chain) = $t[1] =~ /\[(\w+)\]/; my $d3info = "";
         map { 
             my @m = split / /, $_; if ( $t[0] eq $m[1] ) { $d3info = $_; } 
-        } split /\|/, $t[15]; 
-        my $drug = join( "\|", keys %{$ss{$t[0]}{$chain}{$t[2]}{'DRUG'}} ); 
-        my $drugid = join( "\|", keys %{$ss{$t[0]}{$chain}{$t[2]}{'DRUGID'}} );
-        $sub_fh_output->print( join( "\t", $drug, $drugid, @t[0..14], $d3info )."\n" ); 
-    } <$sub_fh_target>;
-    $sub_fh_output->close;
-    $sub_fh_target->close;
-    $sub_fh_drugport_parsing->close;
-    die "Could not open drug nontarget output file\n" unless( $sub_fh_nontarget->open( "$output_prefix.drugs.nontarget" ) );
     die "Could not open drugprot parsing output file\n" unless( $sub_fh_drugport_parsing->open( "$drugport_parsing_results" ) );
     my $sub_fh_nontarget_output = new FileHandle;
     die "Could not create clean nontarget drug output file\n" unless( $sub_fh_nontarget_output->open( ">$output_prefix.drugs.nontarget.clean" ) );
-    $sub_fh_nontarget_output->print( join( "\t", "Drug", "Drugport_ID", "PDB_ID", "Drug_Chain", "Compound_Location", "Res_Name", "Gene", "Chromosome", "Start", "Stop", "Amino_Acid_Change", "Res_Chain", "Mutation_Location_In_PDB", "Res_Name", "Domain_Annotation", "Cosmic_Annotation", "Linear_Distance_Between_Drug_and_Mutation", "3D_Distance_Information\n" ) );
+    $sub_fh_nontarget_output->print( join( "\t", "Drug", "Drugport_ID", "PDB_ID", "Chain", "Compound_Location", "Res_Name", "Gene", "Chromosome", "Start", "Stop", "Amino_Acid_Change", "Chain", "Mutation_Location_In_PDB", "Res_Name", "Domain_Annotaiton", "Cosmic_Annotation", "Linear_Distance_Betweeen_Drug_and_Mutation", "3D_Distance_Information\n" ) );
     undef %ss; map { 
         chomp; my @t = split /\t/; unless ( $t[8] =~ /NULL/ ) {
             map { 
