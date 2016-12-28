5b8d49ef4956a9fc90fc37552e3417f2d67c5961
Amila_Weerasinghe <amila@wustl.edu>
2016-12-28 12:05:40 -0600

    Adding facets to the Membership Probability Plot

--- lib/TGI/Mutpro/Main/MembershipProbability.R
+++ lib/TGI/Mutpro/Main/MembershipProbability.R
@@ -8,7 +8,7 @@ library(ggplot2)
 d$Variant <- factor(d$Variant, levels = d$Variant)
 
 sp <- ggplot(d, aes(x=Variant,y=ClusterID)) + geom_point(aes(colour = Probability), size=3) + scale_colour_gradient(low = "red", high = "blue")
-
+sp <- sp+ facet_grid(SuperClusterID ~ .,as.table = FALSE,scales="free_y",space="free_y")
 #sp <- ggplot(d, aes(x=Variant,y=ClusterID)) + geom_point(aes(size=Probability))
 
 sp+theme_bw() + theme(axis.text.x=element_text(angle=90, size=6)) + ggtitle(paste("Cluster Membership Probabilities for",args[2],"runs (",args[3],")"))
