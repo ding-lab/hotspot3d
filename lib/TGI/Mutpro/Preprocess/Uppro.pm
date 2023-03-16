8c2b7b6ee978f0db68875b07e6e1a3b39c0c1436
Fernanda Martins Rodrigues <fernanda@wustl.edu>
2023-03-16 14:55:48 -0500

    Updated Uppro.pm to handle parsing of AlphaFold DB v4 data.

--- lib/TGI/Mutpro/Preprocess/Uppro.pm
+++ lib/TGI/Mutpro/Preprocess/Uppro.pm
@@ -1,11 +1,12 @@
 package TGI::Mutpro::Preprocess::Uppro;
 #
 #----------------------------------
-# $Authors: Beifang Niu and Adam D Scott
+# $Original authors: Beifang Niu and Adam D Scott
+# $Modified by: Fernanda Martins Rodrigues @WashU (fernanda@wustl.edu; mrodrigues.fernanda@gmail.com)
 # $Date: 2014-01-14 14:34:50 -0500 (Tue Jan 14 14:34:50 CST 2014) $
-# $Revision: 2016-07-27 $
+# $Revision: 2023-03-15 $
 # $URL: $
-# $Doc: $ create & update proximity files (the first step of preprocessing procedure)
+# $Doc: $ create & update proximity files (the first step of preprocessing procedure). This has been adapted on Mar 15 2023 to handle AlphaFold DB (v4) files
 #----------------------------------
 #
 use strict;
@@ -29,6 +30,7 @@ use TGI::Files::List;
 my $MINDISTANCE = "shortest";
 my $AVGDISTANCE = "average";
 my $BSUB = "bsub";
+my $BSUBC1 = "compute1";
 my $LOCAL = "local";
 my $NONE = "none";
 
@@ -41,7 +43,8 @@ sub new {
         'min_seq_dis' => 0,
         'hold' => 0,
         'status' => undef,
-        'pdb_file_dir' => undef,
+        # MODIFIED ON 03-15-2023: changed pdb_file_dir to alphafolddb,
+        'alphafolddb_file_dir' => undef,
         'genes' => undef,
         'distance_measure' => undef,
         #'drugport_file' => undef, 
@@ -63,7 +66,8 @@ sub process {
         '3d-distance-cutoff=i'    => \$this->{'max_3d_dis'},
         'linear-distance-cutoff=i'   => \$this->{'min_seq_dis'},
         'output-dir=s'    => \$this->{'output_dir'},
-        'pdb-file-dir=s'  => \$this->{'pdb_file_dir'},
+        # MODIFIED ON 03-15-2023: changed pdb_file_dir to alphafolddb,
+        'alphafolddb-file-dir=s'  => \$this->{'alphafolddb_file_dir'},
         'gene-file=s'  => \$this->{'genes'},
         'measure=s'  => \$this->{'distance_measure'},
         #'drugport-file=s' => \$this->{'drugport_file'},
@@ -75,21 +79,22 @@ sub process {
     );
     if ($help) { print STDERR help_text(); exit 0; }
     unless ($options) { die $this->help_text(); }
-    #map{ unless($this->{$_} and (-e $this->{$_})) { warn " $_ does not exist ! \n"; die $this->help_text(); } } qw( output_dir pdb_file_dir drugport_file );
     map{
 		unless($this->{$_} and (-e $this->{$_})) {
 			warn " $_ does not exist ! \n";
 			die $this->help_text();
 		}
-	} qw( output_dir pdb_file_dir );
+	# MODIFIED ON 03-15-2023: changed pdb_file_dir to alphafolddb,
+	} qw( output_dir alphafolddb_file_dir );
 	if ( defined $this->{'parallel'} ) {
 		if ( $this->{'parallel'} eq $LOCAL ) {
 			if ( not defined $this->{'max_processes'} ) {
 				die "HotSpot3D::Uppro error: local parallelization requested, but max-processes was not set\n    CAUTION: make sure you know your max CPU processes !\n";
 			}
-		} elsif ( $this->{'parallel'} ne $BSUB and $this->{'parallel'} ne $NONE ) {
+		} elsif ( $this->{'parallel'} ne $BSUB and $this->{'parallel'} ne $NONE and $this->{'parallel'} ne $BSUBC1 ) {
 			my $error = "HotSpot3D::Uppro error: parallelization type not set, please use either: \n";
 			$error .= "\tbsub (if you have an bsub/LSF server), \n";
+			$error .= "\tcompute1 (if you want to use the LSF server on compute1 - only applicable to users members of the Ding Lab at WashU, \n";
 			$error .= "\tlocal (if you want to parallelize on your CPU), or \n";
 			$error .= "\tnone (if you want to run calpro for each protein serially) \n";
 			die $error;
@@ -97,20 +102,23 @@ sub process {
 	} elsif ( not defined $this->{'parallel'} ) {
 		my $error = "HotSpot3D::Uppro error: parallelization type not set, please use either: \n";
 		$error .= "\tbsub (if you have an bsub/LSF server), \n";
+		$error .= "\tcompute1 (if you want to use the LSF server on compute1 - only applicable to users members of the Ding Lab at WashU, \n";
 		$error .= "\tlocal (if you want to parallelize on your CPU), or \n";
 		$error .= "\tnone (if you want to run calpro for each protein serially) \n";
 		die $error;
 	}
     my $pro_dir = "$this->{'output_dir'}\/proximityFiles";
     my $inpro_dir = "$pro_dir\/inProgress";
-    my $pdbcor_dir = "$pro_dir\/pdbCoordinateFiles";
-    my $log_file = "$this->{'output_dir'}\/hugo.uniprot.pdb.csv";
+    # MODIFIED ON 03-15-2023: changed pdbcor_dir to alphafolddbcor_dir and logfile name from hugo.uniprot.pdb.csv to hugo.uniprot.alphafolddb.csv
+    my $alphafolddbcor_dir = "$pro_dir\/alphafolddbCoordinateFiles";
+    my $log_file = "$this->{'output_dir'}\/hugo.uniprot.alphafolddb.csv";
     my $log_dir = "$this->{'output_dir'}\/Logs\/";
     my $measure = $this->{'distance_measure'};
     unless (-e $pro_dir) { mkdir($pro_dir) || die "HotSpot3D Uppro Error: can not make $pro_dir !\n"; }
     unless (-e $log_file) { if (system("touch $log_file") != 0) { die "HotSpot3D Uppro Error: can not make hugo uniprot file !\n"; } }
     unless (-e $inpro_dir) { mkdir($inpro_dir) || die "HotSpot3D Uppro Error: can not make $inpro_dir !\n"; }
-    unless (-e $pdbcor_dir) { mkdir($pdbcor_dir) || die "HotSpot3D Uppro Error: can not make $pdbcor_dir !\n"; }
+    # MODIFIED ON 03-15-2023: changed pdbcor_dir to alphafolddbcor_dir
+    unless (-e $alphafolddbcor_dir) { mkdir($alphafolddbcor_dir) || die "HotSpot3D Uppro Error: can not make $alphafolddbcor_dir !\n"; }
     unless ( -e $log_dir ) { mkdir( $log_dir ) || die "HotSpot3D Uppro Error: can not make $log_dir !\n"; }
 	unless ( $measure eq $MINDISTANCE or $measure eq $AVGDISTANCE ) {
 		die "HotSpot3D::Uppro error: measure not set or recognized (use either shortest or average)\n";
@@ -121,7 +129,8 @@ sub process {
     my $fh = new FileHandle;
     unless ($fh->open(">$log_file")) { die "Could not open hugo uniprot file !\n" };
 	print STDOUT "Creating ".$log_file."\n";
-    my ($hugo_id, $alias_ref, $previous_ref, $alias_list, $uniprot_id, $uniprot_ref, $pdb_ref);
+	# MODIFIED ON 03-15-2023: changed pdb_ref to alphafolddb_ref
+    my ($hugo_id, $alias_ref, $previous_ref, $alias_list, $uniprot_id, $uniprot_ref, $alphafolddb_ref);
 	my $hugogene_ref;
 	my ( $list , @fields );
 	if ( $this->{'genes'} ) { 
@@ -148,8 +157,9 @@ sub process {
 			next;
 		}
         $uniprot_ref = TGI::Mutpro::Preprocess::Uniprot->new($uniprot_id);
-        $pdb_ref = $uniprot_ref->annotations("PDB");
-        if (!defined $pdb_ref || scalar(@{$pdb_ref}) == 0) {
+        # MODIFIED ON 03-15-2023: changed pdb_ref to alphafolddb_ref
+        $alphafolddb_ref = $uniprot_ref->annotations("AlphaFoldDB");
+        if (!defined $alphafolddb_ref || scalar(@{$alphafolddb_ref}) == 0) {
 			$fh->print( "$hugo_id\t$uniprot_id\tN/A\t$alias_list\n" );
 			next;
 		}
@@ -158,14 +168,14 @@ sub process {
 		}
         $fh->print("$hugo_id\t$uniprot_id\t");
         map {
-			my ($pdb_id) = $_ =~ /^(\w+)\;/;
-			if (defined $pdb_id) {
-				if (!defined $$uniprot_to_structureref{$uniprot_id}{$pdb_id}) {
+			my ($alphafolddb_id) = $_ =~ /^(\w+)\;/;
+			if (defined $alphafolddb_id) {
+				if (!defined $$uniprot_to_structureref{$uniprot_id}{$alphafolddb_id}) {
 					$uniprotid_toupdate{$uniprot_id} = 1;
 				}
-				$fh->print("$pdb_id ");
+				$fh->print("$alphafolddb_id ");
 			}
-		} @{$pdb_ref};
+		} @{$alphafolddb_ref};
         $fh->print( "\t$alias_list\n" );
     }
     $fh->close();
@@ -175,6 +185,9 @@ sub process {
 	my $command = "";
 	if ( $this->{'parallel'} eq $BSUB ) {
 		$command = $this->makeBSUBCalproCommand( $inpro_dir , $cmd_list_submit_file_fh , \%uniprotid_toupdate );
+	# MODIFIED ON 03-15-2023: added BSUB for compute1 option - specific to dinglab folks 
+	} elsif ( $this->{'parallel'} eq $BSUBC1 ) {
+		$command = $this->makeBSUBCalproCommandCompute1( $inpro_dir , $cmd_list_submit_file_fh , \%uniprotid_toupdate );
 	} elsif ( $this->{'parallel'} eq $LOCAL ) {
 		$command = $this->makeLocalCalproCommand( $inpro_dir , $cmd_list_submit_file_fh , \%uniprotid_toupdate );
 	} else {
@@ -192,7 +205,7 @@ sub makeCalproCommand {
 	my ( $this , $protein ) = @_;
 	my $command = "hotspot3d calpro ";
 	$command .= "--output-dir=".$this->{'output_dir'}." ";
-	$command .= "--pdb-file-dir=".$this->{'pdb_file_dir'}." ";
+	$command .= "--alphafolddb-file-dir=".$this->{'alphafolddb_file_dir'}." ";
 	$command .= "--uniprot-id=".$protein." ";
 	$command .= "--3d-distance-cutoff=".$this->{'max_3d_dis'}." ";
 	$command .= "--linear-cutoff=".$this->{'min_seq_dis'}." ";
@@ -213,6 +226,20 @@ sub makeBSUBCalproCommand {
 	} keys %{$uniprotid_toupdate};
 }
 
+# MODIFIED ON 03-15-2023: Added code to create BSUB commands suitable for compute1 - specific to dinglab folks only
+sub makeBSUBCalproCommandCompute1 {
+	my ( $this , $inpro_dir , $cmd_list_submit_file_fh , $uniprotid_toupdate ) = @_;
+    my $log_dir = "$this->{'output_dir'}\/Logs\/";
+	map {
+		system("touch $inpro_dir/$_.ProximityFile.csv");
+		my $command = "";
+		$command = "bsub -oo ".$log_dir.$_.".calpro.log -R 'select[type==LINUX64 && mem>16000] rusage[mem=16000]' -M 16000000 -q general -G compute-dinglab -a 'docker(registry.gsc.wustl.edu/genome/genome_perl_environment)'";
+		$command .= "'".$this->makeCalproCommand( $_ )."'";
+		print STDOUT $command."\n"; 
+		$cmd_list_submit_file_fh->print( $command."\n" );
+	} keys %{$uniprotid_toupdate};
+}
+
 sub makeSerialCalproCommand {
 	my ( $this , $inpro_dir , $cmd_list_submit_file_fh , $uniprotid_toupdate ) = @_;
     my $log_dir = "$this->{'output_dir'}\/Logs\/";
@@ -227,6 +254,7 @@ sub makeSerialCalproCommand {
 	return $serialCommand;
 }
 
+# MODIFIED ON 03-15-2023: updated parameters to reflect pdb to alphafold changes
 sub makeLocalCalproCommand {
 	my ( $this , $inpro_dir , $cmd_list_submit_file_fh , $proteinsToUpdate ) = @_;
     my $log_dir = "$this->{'output_dir'}\/Logs\/";
@@ -241,7 +269,7 @@ sub makeLocalCalproCommand {
 	$parallel .= "\tmy \$pid = \$pm->start and next DATA_LOOP;\n";
 	$parallel .= "\tmy \$cmd = \'hotspot3d calpro ";
 	$parallel .= "--output-dir=".$this->{'output_dir'}." ";
-	$parallel .= "--pdb-file-dir=".$this->{'pdb_file_dir'}." ";
+	$parallel .= "--alphafolddb-file-dir=".$this->{'alphafolddb_file_dir'}." ";
 	$parallel .= "--uniprot-id=\'\.\$protein\.\' ";
 	$parallel .= "--3d-distance-cutoff=".$this->{'max_3d_dis'}." ";
 	$parallel .= "--linear-cutoff=".$this->{'min_seq_dis'}." ";
@@ -277,43 +305,40 @@ sub currentuniprot_files {
     return \%uniprot_ids;
 }
 
+# MODIFIED ON 03-15-2023: changed to take alphafolddb_id instead of pdb_id
 # Return ref to hash with 
-# key = uniprot_id, $pdb_id; 
+# key = uniprot_id, $alphafolddb_id; 
 # value = 1
 sub current_structures {
     my ($this, $logfilef) = @_;
-    my ($uniprot_id, $pdb_id, %uniprot_tostructure, $pdb_list);
+    my ($uniprot_id, $alphafolddb_id, %uniprot_tostructure, $alphafolddb_list);
     my $fh = new FileHandle;
     unless ($fh->open($logfilef)) { die "Could not open hugo uniprot file\n" };
     map {
 		chomp;
-		(undef, $uniprot_id, $pdb_list) = split /\t/, $_;
+		(undef, $uniprot_id, $alphafolddb_list) = split /\t/, $_;
 		unless ($uniprot_id eq "N/A") {
 			map {
 				$uniprot_tostructure{$uniprot_id}{$_} = 1;
-			} split /s+/, $pdb_list;
+			} split /s+/, $alphafolddb_list;
 		}
 	} $fh->getlines;
     $fh->close();
-
     return \%uniprot_tostructure;
 }
-
 sub help_text{
     my $this = shift;
         return <<HELP
-
 Usage: hotspot3d uppro [options]
-
                              REQUIRED
 --output-dir                 Output directory of proximity files
---pdb-file-dir               PDB file directory 
+--alphafolddb-file-dir       AlphaFold DB file directory 
 --measure                    Distance measure between residues (shortest or average)
---parallel                   Parallelization to use (bsub, local, or none)
+--parallel                   Parallelization to use (bsub, compute1, local, or none)
                                  use bsub if you want to use an LSF server
+                                 use compute1 if you want to use the LSF server on compute1 (this is only applicable to users members of the Ding Lab at WashU)
                                  use local if you want to use your CPU (CAUTION: make sure you know your max CPU processes)
                                  use none if you want to run calpro for each protein serially
-
                              OPTIONAL
 --max-processes              Set if using parallel type local (CAUTION: make sure you know your max CPU processes)
 --gene-file                  File with HUGO gene names in the first column (like a .maf)
@@ -321,43 +346,26 @@ Usage: hotspot3d uppro [options]
 --linear-distance-cutoff     Minimum linear distance (> peptides), default: 0
 --cmd-list-submit-file       Batch jobs file to run calpro step in parallel, default: cmd_list_submit_file
 --hold                       Do not submit batch jobs, just write cmd_list_submit_file, default: submits (takes no input)
-
 --help                       this message
-
 HELP
-
 }
-
 1;
-
 __END__
  
 =head1 NAME
-
 TGI::Mutpro::Preprocess::Uppro - Create & update proximity files.
-
 =head1 SYNOPSIS
-
   use TGI::Mutpro::Preprocess::Uppro;
-
 =head1 DESCRIPTION
-
 TGI::Mutpro::Preprocess::Uppro is to be used to create & update proximity files.
 It is the first step of preprocessing procedure.
-
-
 =head1 AUTHOR
-
 Beifang Niu E<lt>beifang.cn@gmail.comE<gt>
-
+For questions regarding HotSpot3d's AlphaFold DB version, please contact Fernanda Martins Rodrigues E<lt>mrodrigues.fernanda@gmail.comE<gt>
 =head1 SEE ALSO
-
-https://github.com/ding-lab/hotspot3d
-
+https://github.com/ding-lab/hotspot3d/
+For version suitable for AlphaFold DB, please see: https://github.com/ding-lab/hotspot3d/blob/alphaFold_implementation
 =head1 LICENSE
-
 This library is free software with MIT licence; you can redistribute it and/or modify
 it under the same terms as Perl itself.
-
 =cut
-
